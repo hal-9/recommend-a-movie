d67a40aa62f3c15b3c188b0173c09931522928c9
Tuncay Aydin <tunikb@posteo.de>
2021-01-22 11:36:04 +0100

    added favorite movie selector

--- js/app.js
+++ js/app.js
@@ -1,13 +1,7 @@
-const grid = document.querySelector('#grid');
-const genreSelector = document.querySelector('#genreSelector');
+const grid = document.querySelector('.grid');
+const genreSelector = document.querySelector('.genreSelector');
 
-const generateList = (selectedGenre) => {
-  const filteredList = data
-    .filter(movie => movie.Genre[0] === selectedGenre)
-    .sort(function(movie1, movie2) {
-      return movie2.Rating - movie1.Rating;
-    });
-
+const createMovieEntries = (filteredList) => {
   filteredList.forEach((movieData) => {
     const gridItem = document.createElement('div')
     gridItem.classList.add('gridItem');
@@ -24,6 +18,23 @@ const generateList = (selectedGenre) => {
     gridItem.appendChild(letterboxdLink);
     letterboxdLink.appendChild(movie);
   });
+}
+
+const generateList = (selectedGenre) => {
+  if (selectedGenre === 'Favorite') {
+    const filteredListFavorite = data
+    .filter(movie => movie.Rating === 5);
+
+    createMovieEntries(filteredListFavorite);
+  } else {
+    const filteredListGenre = data
+      .filter(movie => movie.Genre[0] === selectedGenre)
+      .sort(function(movie1, movie2) {
+        return movie2.Rating - movie1.Rating;
+      });
+    
+      createMovieEntries(filteredListGenre);
+  }
 };
 
 
